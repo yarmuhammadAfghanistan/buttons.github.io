a98263b3aaf8caed4a3d3ab2a30f3d99f5526170
yarmuhammadAfghanistan <mannkhanmanankhan@gmail.com>
2024-04-07 21:48:25 +0500

    first commit

---
+++ first.js
@@ -0,0 +1,55 @@
+
+// function lightmode() {
+
+
+//     const yar = document.getElementById("red")
+//     yar.innerHTML = "hallow calss "
+//     yar.style.color = "white"
+//     yar.style.background = "black"
+
+
+
+
+
+// }
+
+// function darkmode() {
+
+
+//     const yar = document.getElementById("red")
+//     yar.innerHTML = "hallow calss "
+//     yar.style.color = "black"
+//     yar.style.background = "red"
+
+
+
+
+
+// }
+
+// function change(){
+//  document.getElementById("image").setAttribute("src","./pexels-photo-428328 copy.jpg") 
+
+
+
+// }
+
+// const x = Math.random()
+// console.log(x)
+// y = x * (9) + 0
+// console.log(y)
+
+ 
+ 
+function  myfunction() {
+     document.getElementById("yar").className = "yar";
+    
+   };
+   function  myclick() {
+     document.getElementById("khan") .className = "khan";
+     
+   };
+   function  myplick() {
+     document.getElementById("masood").className = "masood";
+     
+   }
